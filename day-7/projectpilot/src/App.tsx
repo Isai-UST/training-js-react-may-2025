dc3b4a7cab8853fb9067329efe81ef3226d78c4d
Isai Hernandez Villalpando <233266@ust.com>
2025-05-20 15:26:41 +0000

    Day 7 (stay in lab 16)

---
+++ day-7/projectpilot/src/App.tsx
@@ -0,0 +1,15 @@
+import ProjectsPage from './projects/ProjectsPage';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <ProjectsPage />
+      </div>
+    </>
+  )
+}
+
+export default App
