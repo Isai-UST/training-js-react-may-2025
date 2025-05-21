be9667b7369f4d2739787c1824e1612927311cd6
Isai Hernandez Villalpando <233266@ust.com>
2025-05-21 19:53:44 +0000

    Day 9 completed Lab 23

--- day-7/projectpilot/src/App.tsx
+++ day-7/projectpilot/src/App.tsx
@@ -1,15 +1,34 @@
 import ProjectsPage from './projects/ProjectsPage';
 import './App.css'
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router';
+import HomePage from './home/HomePage';
+import ProjectPage from './projects/ProjectPage';
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/"  className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
       <div className="container">
-        <ProjectsPage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects/:id" element={<ProjectPage />} />
+        </Routes>
       </div>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
 export default App
