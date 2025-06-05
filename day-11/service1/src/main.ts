8fcb3e55983d460b51e1938e014ef26d663d697f
Isai Hernandez Villalpando <233266@ust.com>
2025-06-05 16:49:43 +0000

    add cors config

--- day-11/service1/src/main.ts
+++ day-11/service1/src/main.ts
@@ -6,8 +6,12 @@ import { TransformInterceptor } from './common/interceptor/interceptor.transform
 import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
-  app.enableCors();
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+            origin: true,
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+            credentials: true,
+        });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
